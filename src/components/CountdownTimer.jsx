014d11a577585b051956be1e5874e95866310fc4
DenysRogeres <dennys.rogeres@gmail.com>
2024-06-26 23:52:03 -0300

    finalizado implementacao parte 1

--- src/components/CountdownTimer.jsx
+++ src/components/CountdownTimer.jsx
@@ -4,15 +4,19 @@ export function CountdownTimer(){
   const [timeLeft, setTimeLeft] = useState(15 * 60);
 
   useEffect(() => {
-    const countdown = () => {
-      if (timeLeft >= 0) {
-        setTimeout(() => {
-          setTimeLeft(timeLeft - 1);
-        }, 1000);
-      }
-    };
-    countdown();
-  }, [timeLeft]);
+    const intervalId = setInterval(() => {
+      setTimeLeft((prevTimeLeft) => {
+        if (prevTimeLeft > 0) {
+          return prevTimeLeft - 1;
+        } else {
+          clearInterval(intervalId);
+          return 0; // Ensure timeLeft stays at 0 when countdown finishes
+        }
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalId); // Cleanup interval on component unmount
+  }, []);
 
   const minutes = Math.floor(timeLeft / 60);
   const seconds = timeLeft % 60;
--- src/pages/Etapa_04.jsx
+++ src/pages/Etapa_04.jsx
@@ -31,7 +31,7 @@ export function Etapa04() {
    );
 
   useEffect(() => {
-    const tenMinutes = 18 * 60 * 1000;;
+    const tenMinutes = 19 * 60 * 1000;;
     const firstVisitTime = localStorage.getItem('firstVisitTime');
 
     if (!firstVisitTime) {
