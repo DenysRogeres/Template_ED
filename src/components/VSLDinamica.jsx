53c69affe071e20fe35f9376d976393839bfc285
DenysRogeres <dennys.rogeres@gmail.com>
2024-06-27 21:46:27 -0300

    adicionando pixel face

--- src/components/VSLDinamica.jsx
+++ src/components/VSLDinamica.jsx
@@ -23,6 +23,34 @@ const VSLDinamica = () => {
     };
   }, []);
 
+
+  useEffect(() => {
+   // Adiciona o Pixel do Facebook
+   !function(f,b,e,v,n,t,s)
+   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+   if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+   n.queue=[];t=b.createElement(e);t.async=!0;
+   t.src=v;s=b.getElementsByTagName(e)[0];
+   s.parentNode.insertBefore(t,s)}(window, document,'script',
+   'https://connect.facebook.net/en_US/fbevents.js');
+   fbq('init', '1017965596599303');
+   fbq('track', 'PageView');
+
+   const noscript = document.createElement('noscript');
+   const img = document.createElement('img');
+   img.height = 1;
+   img.width = 1;
+   img.style.display = 'none';
+   img.src = 'https://www.facebook.com/tr?id=1017965596599303&ev=PageView&noscript=1';
+   noscript.appendChild(img);
+   document.head.appendChild(noscript);
+
+   return () => {
+     document.head.removeChild(noscript);
+   };
+ }, []);
+
   return (
 
 
